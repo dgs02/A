3110ff074ce17d89cb5e56849d35073517b33d64
David <gdavid1004@alumnos.ilernasevilla.com>
2023-09-26 16:48:22 +0200

    Segundo ejercicio

--- 001tres.php
+++ 001tres.php
@@ -2,3 +2,13 @@
 
 echo "hola, esto es un echo";
 print("Hola, esto es un print");
+
+
+
+/*Esto es 
+una prueba de comentario
+en bloque*/
+
+//Esto es una prueba de comentario en linea 
+
+?>
---
+++ 002misDatos.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Datos Personales</title>
+</head>
+<body>
+    <?php
+        //Aquí creamos las varibales y asignamos sus datos
+        $nombre = "David";
+        $primerApellido = "Gonzalez";
+        $segundoApellido = "Saez";
+        $email = "gdavid1004@gmail.com";
+        $anoNacimiento = 2002;
+        $movil = "644958756";
+    ?>
+
+    <h1>Tabla</h1>
+    <table>
+        <tr>
+            <th>Nombre</th>
+            <td><?php echo $nombre; ?></td>
+        </tr>
+        <tr>
+            <th>Primer Apellido</th>
+            <td><?php echo $primerApellido; ?></td>
+        </tr>
+        <tr>
+            <th>Segundo Apellido</th>
+            <td><?php echo $segundoApellido; ?></td>
+        </tr>
+        <tr>
+            <th>Email</th>
+            <td><?php echo $email; ?></td>
+        </tr>
+        <tr>
+            <th>Año de Nacimiento</th>
+            <td><?php echo $anioNacimiento; ?></td>
+        </tr>
+        <tr>
+            <th>Móvil</th>
+            <td><?php echo $movil; ?></td>
+        </tr>
+    </table>
+</body>
+</html>
