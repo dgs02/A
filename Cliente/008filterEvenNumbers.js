cc5ab58561bb4f91a87aa44f46c983a44b4b140b
David <gdavid1004@alumnos.ilernasevilla.com>
2023-10-05 16:41:48 +0200

    8

---
+++ Cliente/008filterEvenNumbers.js
@@ -0,0 +1,11 @@
+
+let filtrarNumeros = (arr) => {
+    evenNumbers = arr.filter((number) => number % 2 === 0);
+    return evenNumbers;
+  };
+  
+  // Ejemplo de uso:
+  numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  numerosPares = filtrarNumeros(numeros);
+  document.write(numerosPares);
+  
