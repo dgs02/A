6a52be4d938a436de70c84bb2eaa3f15f5bd04f2
David <gdavid1004@alumnos.ilernasevilla.com>
2023-10-25 21:29:06 +0200

    012

---
+++ Cliente/Actividad 4/012testCuriosity.js
@@ -0,0 +1,6 @@
+'use strict';
+
+alert( 9999999999999999 );
+
+// En numero 9999999999999999 supera la capacidad de interpretación de javaScript por lo que el
+// programa interpreta el numero mas cercano por el estandar del punto flotante.
