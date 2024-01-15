3823dd2a2f30d9e654793d06d1044bdc36d4d647
David <gdavid1004@alumnos.ilernasevilla.com>
2024-01-15 17:53:31 +0100

    005

---
+++ Cliente/Actividad 4.2 DOM/005hideButton.js
@@ -0,0 +1,9 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+    const hideButton = document.getElementById('hideButton');
+
+    hideButton.addEventListener('click', function() {
+        hideButton.style.display = 'none';
+    });
+});
